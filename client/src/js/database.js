7edf69771c3970ef086fd0f22cd851af68683ed0
JoelhansenMN <joelhansen22@gmail.com>
2024-05-24 20:45:19 -0500

    database.js updates

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -4,16 +4,16 @@
 import { openDB } from 'idb';
 
 // We will define a global constant for our database name so we don't mess it up anywhere
-const DB_NAME = "jate"
+const jatedb = "jate"
 
 const initdb = async () =>
-  openDB(DB_NAME, 1, {
+  openDB(jatedb, 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains(DB_NAME)) {
+      if (db.objectStoreNames.contains(jatedb)) {
         console.log('jate database already exists');
         return;
       }
-      db.createObjectStore(DB_NAME, { keyPath: 'id', autoIncrement: true });
+      db.createObjectStore(jatedb, { keyPath: 'id', autoIncrement: true });
       console.log('jate database created');
     },
   });
@@ -25,15 +25,15 @@ export const putDb = async (content) => {
   // First, create a variable, and set it to asyncronously await the opening of the database. Replace the items in all caps
   
   // TODO: Change YOUR_OPEN_DB_VAR to whatever variable name you wanT. Note that you'll then need to change any other occcurences of YOUR_OPEN_DB_VAR to the same variable name.
-  const YOUR_OPEN_DB_VAR = await openDB(DB_NAME, 1);
+  const open = await openDB(jatedb, 1);
 
   // TODO: Now create a variable for the transaction; again, this will be referenced below.
-  const YOUR_TX_VAR = YOUR_OPEN_DB_VAR.transaction(DB_NAME, 'readwrite');
+  const text = open.transaction(jatedb, 'readwrite');
 
   // TODO: Now create a variable for the store
-  const YOUR_STORE_VAR = YOUR_TX_VAR.objectStore(DB_NAME);
+  const store = text.objectStore(jatedb);
 
-  const request = YOUR_STORE_VAR.put({ id: 1, value: content });
+  const request = store.put({ id: 1, value: content });
   const result = await request;
   console.log('🚀 - data saved to the database', result.value);
 };
@@ -45,7 +45,9 @@ export const getDb = async () => {
   // You can duplicate the same lines of code from above, except that the transaction will be 'readonly'
   
   // TODO: Copy LINES 28, 31 and 34 above; the new line 31 code should be "readonly"
-
+  const open = await openDB(jatedb, 1);
+  const text = open.transaction(jatedb, 'readonly');
+  const store = text.objectStore(jatedb);
   // Leave the rest as-is
   const request = store.get(1);
   const result = await request;
